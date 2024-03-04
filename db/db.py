67117ae863fd5f6d57a595fd8aef4bb1b7a3eaba
nathan-beyero <nathanbeyero@gmail.com>
2024-03-04 15:42:56 -0500

    Update database host

--- db/db.py
+++ db/db.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, SQLModel, Session, select
 from models.models import Item, Product
 
 conf = {
-    'host': "localhost",
+    'host': "0.0.0.0",
     'port':'5432',
     'database': "postgres",
     'user': "mydatabase1",
